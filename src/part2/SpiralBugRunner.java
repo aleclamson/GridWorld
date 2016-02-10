b2b68cee97afd5c0d4e775471992420467ab7aca
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-02-10 10:05:11 -0700

    spiral bug

---
+++ src/part2/SpiralBugRunner.java
@@ -0,0 +1,18 @@
+package part2;
+
+import java.awt.Color;
+
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Location;
+
+public class SpiralBugRunner {
+	public static void main(String[] args) {
+		ActorWorld world = new ActorWorld();
+		SpiralBug alice = new SpiralBug(1);
+		alice.setColor(Color.ORANGE);
+		SpiralBug bob = new SpiralBug(3);
+		world.add(new Location(7, 8), alice);
+		world.add(new Location(5, 5), bob);
+		world.show();
+	}
+}
