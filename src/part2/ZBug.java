e1e97f19d91cbebc28bfb215b35933a107ce2251
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-02-10 11:01:18 -0700

    started zbug

---
+++ src/part2/ZBug.java
@@ -0,0 +1,12 @@
+package part2;
+
+public class ZBug extends Bug{
+	private int steps;
+	private int sidelength;
+	private boolean Zdone = false;
+	
+}
+	public int ZBug (sidelength){
+		sidelength = length;
+		
+	}
